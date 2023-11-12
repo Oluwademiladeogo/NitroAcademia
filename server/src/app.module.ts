d8f9444e0ef3bd998e7a2cd29089465cfece540d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-12 22:37:15 +0100

    initial commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SignupController } from './signup/controller/signup/signup.controller';
+import { UsersService } from './services/users/users.service';
+import { LoginController } from './login/controller/login/login.controller';
+import { CoursesController } from './courses/controller/courses/courses.controller';
+import { CoursesService } from './services/courses/courses.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, SignupController],
-  providers: [AppService],
+  controllers: [AppController, SignupController, LoginController, CoursesController],
+  providers: [AppService, UsersService, CoursesService],
 })
 export class AppModule {}
