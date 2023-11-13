577f4a8f6244060dae0ebd1edf6790a2a6bbb5c2
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-13 14:39:28 +0100

    chore: minor code changes

--- server/src/login/controller/login/login.controller.ts
+++ server/src/login/controller/login/login.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Response } from 'express';
+import { authHelper } from 'src/helpers/auth.helper';
+import { loginUserDto } from 'src/login/dtos/loginUser.dto';
+import { UsersService } from 'src/services/users/users.service';
 
 @Controller('login')
-export class LoginController {}
+export class LoginController {
+  constructor(
+    private UsersService: UsersService,
+    private authHelper: authHelper,
+  ) {}
+  @UsePipes(new ValidationPipe())
+  @Post()
+  async loginUser(@Body() data: loginUserDto, @Res() res: Response) {
+    const user: any = await this.UsersService.findUserByEmail(data.email);
+    const pw = await this.authHelper.compare(data.password, user.password);
+    if (!pw || !user[0])
+      return res.json({ status: 400, message: 'invalid email or password' });
+    res.json({ status: 200, message: 'User successfully logged in' });
+  }
+}
