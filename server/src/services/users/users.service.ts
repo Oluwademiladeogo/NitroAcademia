0eac8bcb094130abefca265d65f38ac0508b4cff
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-11-13 10:39:57 +0100

    create and finduserbyEmail functions

--- server/src/services/users/users.service.ts
+++ server/src/services/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/Users';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async createUser(data: object) {
+    await this.userRepository.save(data);
+  }
+  async findUserByEmail(email:string){
+    return await this.userRepository.findBy({email: email})
+  }
+}
